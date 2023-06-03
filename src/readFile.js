1bde11dbf54ac464ac9d6b2c3b69d9c58363918a
Enes <satrancmenes@gmail.com>
2023-06-04 00:48:29 +0300

    small changes

--- src/readFile.js
+++ src/readFile.js
@@ -1,11 +1,29 @@
-parts = document.getElementsByClassName("part-bg")
-// console.log(parts[0])
-
-document.onload = readTextFrom("/content/week1.txt")
-.then(response => response.text())
-.then(text => placeTextIntoDiv(parts[0], text))
-.catch(error => console.log(error))
-
+document.addEventListener('click', function(e) {
+    e = e || window.event;
+    var target = e.target || e.srcElement,
+        text = target.textContent || target.innerText;
+    if(target.nodeName === "BUTTON")
+    {
+        var partName = target.parentElement.parentElement.className
+        console.log(partName)
+        if(target.className === "plus-button"){
+            console.log("plus")
+            console.log(target)
+            readTextFrom("/content/week1.txt")
+            .then(response => response.text())
+            .then(text => placeTextIntoDiv(target.parentElement.parentElement.nextElementSibling, text))
+            .catch(error => console.log(error))
+             
+        }
+        else if(target.className === "minus-button")
+        {
+            target.parentElement.parentElement.nextElementSibling.innerHTML = ""
+            console.log("minus")
+        }
+    }
+          
+     
+}, false);
 
 function placeTextIntoDiv(div, text)
 {
